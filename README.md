65a2b58da314912848e5203cc8a6187c0ca432b7
Ralph Depping <github@depping.me>
2022-03-21 21:22:34 +0000

    updated for python3 and better error handling

--- README.md
+++ README.md
@@ -1,26 +1,39 @@
 # mp3Convert
+
+## About
 Convert mp3 files to a set "profile"
 
-`mp3convert` is a simple wrapper around ffmpeg that will convert mp3 files found in a source directory.
+`mp3convert` is a simple wrapper around [ffmpeg](https://www.ffmpeg.org/ffmpeg.html) that will convert mp3 files found in a source directory tree.
 
-* Files are found in the source directory based on extension (.mp3 or .MP3).
-* It will also convert files in aac with the .aac extension
+* Files are found in the source directory structure recursively based on specific extensions (.mp3 or .MP3 or .aac).
 * The destination directory will be created if it does not already exist.
 
-The source and destination directories are specified on the command line
-```
-python mp3Convert.py src dest
-```
-It's possible to specify a dryRun as well by adding True
-```
-python mp3Convert.py src dest True
-```
-The conversion parameters for ffmpeg are currently hardcoded in `mp3Convert`.
-
-```
--acodec libmp3lame -vn -ar 44100 -ac 2 -ab 64k -vol 400 -f mp3 -threads 4
-```
 The script also uses [Fire CLI](https://github.com/google/python-fire) in order
 to expose the input parameters on the command line automatically.
 
-The script was originally developed for converting the audio files for the [Irish Preachers Conference](http://www.irishpreachers.org) 
+The script was originally developed for converting the audio files for the [Irish Preachers Conference](https://www.irishpreachers.org) and the original windows bat file is included for reference.
+
+## Setup
+
+I recommend setting up a virtual environment using python3 and installing the requirements as follows:
+
+```bash
+virtualenv venv -p python3
+source venv/bin/activate
+pip install -r requirements
+```
+
+## How To Run
+Simply call the script and specify the src directory tree containing the files to convert and an output directory. Note that the source directory structure is NOT preserved (so all source filenames need to be unique).
+```
+python mp3Convert.py src dest
+```
+It's possible to specify a dryRun as well by adding True. This will print out all the conversion commands that would be run
+```
+python mp3Convert.py src dest True
+```
+The conversion parameters for ffmpeg are currently hardcoded in `mp3Convert` and can be set there as needed prior to running the script.
+
+```
+-acodec libmp3lame -vn -ar 44100 -ac 2 -ab 64k -filter:a "volume=1.5" -f mp3
+```
--- mp3Convert.py
+++ mp3Convert.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 ###############################################################################
 # mp3Convert
@@ -6,42 +6,44 @@
 # Converts audio files to mp3 using a fixed set of parameters
 # - ensures that a set of mp3 (or aac) files are converted to a set mp3 profile
 ###############################################################################
+
 import fire
 from ffmpy import FFmpeg
 import os
+from pathlib import Path
 
 
-def convertMp3(srcDir, destDir, dryRun=False):
+def mp3convert(src_dir, dest_dir, dry_run=False):
 
-    conversionParams = '-acodec libmp3lame -vn -ar 44100 -ac 2 -ab 64k\
-        -vol 400 -f mp3 -threads 4'
+    # 44khz stereo at 64kb
+    conversion_params = '-acodec libmp3lame -vn -ar 44100 -ac 2 -ab 64k -filter:a "volume=1.5" -f mp3'
 
-    for inputFile in os.listdir(srcDir):
-        if inputFile.endswith(".mp3") or inputFile.endswith(".MP3") or \
-                inputFile.endswith(".aac"):
+    for input_file in list(Path(src_dir).rglob("*.[mMa][pPa][3c]")):
 
-            # We found one file to convert, ensure the output directory exists
-            if not os.path.exists(destDir):
-                os.makedirs(destDir)
+        # We found at least one file to convert, ensure the output directory exists
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
 
-            # Ensure the file extensiton for the target is mp3
-            outputFile = os.path.splitext(inputFile)[0]+'.mp3'
+        # Ensure the final file extension for the target is lowercase mp3
+        output_file = os.path.join(dest_dir, input_file.stem + '.mp3')
+        if Path(output_file).exists():
+            print(f'{output_file} already exists in destination path skipping...')
+            continue
 
-            ff = FFmpeg(
-                inputs={os.path.join(srcDir, inputFile): None},
-                outputs={os.path.join(destDir, outputFile): conversionParams})
+        ff = FFmpeg(
+            inputs={os.path.join(src_dir, input_file): None},
+            outputs={output_file: conversion_params})
 
-            print('Command line : {cmd}')\
-                .format(cmd=ff.cmd)
+        print(f'Command line : {ff.cmd}')
 
-            if dryRun is False:
-                ff.run()
+        if dry_run is False:
+            ff.run()
 
 
 def main():
     # Fire exposes the input parameters to the convert method via the command
     # line
-    fire.Fire(convertMp3)
+    fire.Fire(mp3convert)
 
 
 if __name__ == '__main__':
